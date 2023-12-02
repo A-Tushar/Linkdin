00d64234d8623610eb275118b59373a0b80c6549
A-Tushar <mishurahman2022@gmail.com>
2023-12-03 03:16:00 +0600

    post done

--- src/components/Postbox.jsx
+++ src/components/Postbox.jsx
@@ -1,12 +1,53 @@
 import React, { useState, useRef } from 'react'
 import { CiImageOn } from "react-icons/ci";
+import { getDatabase, ref,onValue, set,push, remove } from "firebase/database";
+import { getStorage, ref as imgref,uploadBytes,getDownloadURL, } from "firebase/storage";
+import { useSelector } from 'react-redux';
 
 const Postbox = () => {
+    const db = getDatabase();
+    const storage = getStorage();
+    let userinfo = useSelector((state)=>(state.active.value));
+    let [requeslist,setRequestlist]=useState([]);
+    let [postdata,setPostdata]=useState('')
+    let [imgurl,setImgurl]=useState("");
 
-    const fileInputRef = useRef(null);
-    const handleButtonClick = () => {
-        fileInputRef.current.click();
-      };
+    // const fileInputRef = useRef(null);
+    // const handleButtonClick = () => {
+    //     fileInputRef.current.click();
+    //   };
+
+    let handlepost=()=>{
+        if(postdata.length!=0){
+
+        set(push(ref(db, 'posts/')), {
+            caption:postdata,
+            ownerid:userinfo.uid,
+            ownername:userinfo.displayName,
+            imgurl:imgurl,
+        });
+
+        setPostdata('')
+        }
+        
+    };
+
+    let handpostinfo=(e)=>{
+        setPostdata(e.target.value)
+    }
+
+    let handleimage =(e)=>{
+        
+        const storageRef = imgref(storage, e.target.files[0].name);
+        uploadBytes(storageRef, e.target.files[0]).then((snapshot) => {
+        // console.log('Uploaded a blob or file!');
+        getDownloadURL(storageRef).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            setImgurl(downloadURL);
+          });
+          
+        });
+    }
 
   return (
    <>
@@ -15,10 +56,13 @@ const Postbox = () => {
         <h4>Create a Post </h4>
     </div>
     <div className='p-5 flex items-center gap-3 '>
-        <input className='focus:outline-none border-none p-7 w-3/4' type="text" placeholder='What’s on your mind?' />
-        <CiImageOn onClick={handleButtonClick} className='font-black text-5xl ' />
-        <input ref={fileInputRef} className='hidden' type="file" />
-        <button
+        <input onChange={handpostinfo} value={postdata} name='caption' className='focus:outline-none border-none p-7 w-3/4' type="text" placeholder='What’s on your mind?' />
+
+        <label>
+        <CiImageOn  className='font-black text-5xl ' />
+        <input onChange={handleimage} className='hidden' type="file" accept='image/*' />
+        </label>
+        <button onClick={handlepost}
        className='border-2 border-solid text-white bg-sky-800 rounded-xl text-center box-border hover:bg-sky-400 duration-300 px-5 py-2 '>Post</button>
     </div>
    </div>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from './Image'
 import bg from '../assets/cover.jpg'
+import { getDatabase, ref,onValue, set,push,remove } from "firebase/database";
+import { useSelector } from 'react-redux';
+
+
 const Posts = () => {
+
+  const db = getDatabase();
+  let userdata = useSelector((state)=>(state.active.value));
+  let [posts,setPosts]=useState([]);
+ 
+
+  useEffect(()=>{
+    
+    const userRef = ref(db, 'posts');
+    onValue(userRef, (snapshot) => {
+    let arr=[]
+    snapshot.forEach(item=>{
+      arr.push(item.val());      
+    })
+
+    setPosts(arr);
+
+    });
+    },[]);
+
+
   return (
     <>
-  <div className="w-11/12">
-    <h1>hello </h1>
-    <Image src={bg} classname={"w-40 h-40 object-cover"}/>
-  </div>
+    {posts.map(item=>
+      
+      <div className="w-11/12 m-6 border border-gray-400 rounded-xl p-5 ">
+        <div className="flex items-center">
+          <Image classname={'w-14 h-14 object-cover rounded-full mr-4'} src={bg}/>
+          <h2 className="text-xl font-extrabold font-main-font text-black">{item.ownername}</h2>
+        </div>
+        <h1 className='font-main-font text-base font-normal capitalize px-20 my-5 '>{item.caption}</h1>
+        
+        {item.imgurl && <Image src={item.imgurl} classname={"mx-auto w-[300px] h-[300px] object-cover"}/>}
+
+      </div>
+
+      )}
+
+     
     </>
   )
 }
