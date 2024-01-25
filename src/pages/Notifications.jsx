cb8469b699df28050b2acbdaefae5ac5d9a1ee0f
A-Tushar <mishurahman2022@gmail.com>
2024-01-26 02:04:02 +0600

    ...

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-
+import Image from '../components/Image';
+import logo from '../assets/ex.png'
 
 const Notifications = () => {
 
@@ -11,7 +12,14 @@ const Notifications = () => {
       <h2 className=' font-main-font font-extrabold text-2xl text-amber-950'>Notifications</h2>
     </div>
     <div className="mx-auto w-[650px] my-2 px-5 py-2 bg-slate-100">
-      t
+      <div className="flex items-center gap-3">
+            <Image src={logo} classname={'w-14 h-14 rounded-full'}/>
+            <div className="">
+            <h4 className=' font-main-font capitalize font-bold text-lg'> Parbej hasan</h4>
+            <h4 className=' font-main-font capitalize font-medium text-lg'> sent you a friend Request </h4>
+            </div>
+      </div>
+
     </div>
     </>
   )
